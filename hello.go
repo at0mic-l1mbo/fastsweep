53268396c945ae5037b9359f41757d6a0a6f210b
at0mic-l1mbo <augustolazer@gmail.com>
2024-04-03 18:43:10 -0300

    finished

---
+++ hello.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+	"github.com/fatih/color"
+)
+
+func pingHost(host string, wg *sync.WaitGroup, arq *os.File) {
+	defer wg.Done()
+
+	full_command := fmt.Sprintf("ping -c 1 -w 1 %s | grep '64 bytes'", host)
+	_, err := exec.Command("/bin/bash", "-c", full_command).Output()
+	if err == nil {
+		message := fmt.Sprintf("[+] Response from %s\n", host)
+		data := []byte(message)
+		arq.Write(data)
+	}
+}
+
+func beginPing(host string, arq *os.File) {
+	var wg sync.WaitGroup
+
+	for octet := 1; octet <= 256; octet++ {
+		full_host := fmt.Sprintf("%s.%d", host, octet)
+		wg.Add(1)
+		go pingHost(full_host, &wg, arq)
+	}
+
+	wg.Wait()
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		color.Red("[-] Invalid usage!")
+		fmt.Println("Usage: ./fastsweep 192.168.0")
+		//fmt.Println("Usage: go hello.go 192.168.0")
+	} else {
+		arq, err := os.Create("results.txt")
+		if err != nil {
+			log.Fatal("Failed to create file, verify your OS permissions!")
+		}
+		host := os.Args[1]
+		color.Green("[+] Started ping sweep, could take a minute!")
+		beginPing(host, arq)
+		defer arq.Close()
+		color.Green("[+] Results are saved in {results.txt}")
+	}
+}
